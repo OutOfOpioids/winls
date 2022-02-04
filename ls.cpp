17e346fa5c331a40bed76ddb4869565ea264b162
Jan <41554065+NotKronos@users.noreply.github.com>
2022-02-04 22:10:39 +0100

    added recursive mode

--- ls.cpp
+++ ls.cpp
@@ -18,7 +18,7 @@ bool endsWith(string const& fullString, string const& ending) {
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     string path = fs::current_path().string();
     if (argc == 1) {
@@ -40,12 +40,32 @@ int main(int argc, char *argv[]) {
 
         }
     }
-    else {
-        if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
-            cout << "Usage: ls [FILE]" << endl;
-            cout << "List files in a directory" << endl;
-            cout << "Source code: https://github.com/NotKronos/winls" << endl;
+    else if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
+        cout << "Usage: ls [FILE]" << endl;
+        cout << "List files in a directory" << endl;
+        cout << "Source code: https://github.com/NotKronos/winls" << endl;
+    }
+    else if (strcmp(argv[1], "--recursive") == 0 || strcmp(argv[1], "-R") == 0) {
+        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+        string path = fs::current_path().string();
+        for (const auto& entry : fs::recursive_directory_iterator(path)) {
+            file = entry.path().string();
+            file.erase(file.begin(), file.begin() + path.size() + 1);
+            if (fs::is_directory(entry)) {
+                SetConsoleTextAttribute(hConsole, 9);
+                cout << file + "/" << endl;
+            }
+            else if (endsWith(file, ".exe")) {
+                SetConsoleTextAttribute(hConsole, 12);
+                cout << file << endl;
+            }
+            else {
+                SetConsoleTextAttribute(hConsole, 15);
+                cout << file << endl;
+            }
+
         }
     }
     SetConsoleTextAttribute(hConsole, 15);
 }
+
