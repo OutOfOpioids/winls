d4502e6b00eb252806a0eec59eb8689c52f32505
jan <jan.kuczaj@gmail.com>
2022-10-04 19:59:32 +0200

    went through the todo list

--- ls.cpp
+++ ls.cpp
@@ -1,19 +1,12 @@
-/*
-*TODO: fix the same exact code appearing in two different places
-*TODO: make it loop through args, makes using multiple args at the same time possible
-*/
 #include <iostream>
 #include <filesystem>
 #include <direct.h>
 #include <string>
 #include <Windows.h>
 
-using namespace std;
-namespace fs = std::filesystem;
+std::string file;
 
-string file;
-
-bool endsWith(string const& fullString, string const& ending) {
+bool endsWith(std::string const& fullString, std::string const& ending) {
     if (fullString.length() >= ending.length()) {
         return (0 == fullString.compare(fullString.length() - ending.length(), ending.length(), ending));
     }
@@ -22,54 +15,52 @@ bool endsWith(string const& fullString, string const& ending) {
     }
 }
 
+void listFiles(const auto& entry, const std::string& path, HANDLE hConsole) {
+    file = entry.path().string();
+    file.erase(file.begin(), file.begin() + path.size() + 1); // NOLINT(cppcoreguidelines-narrowing-conversions)
+    if (std::filesystem::is_directory(entry)) {
+        SetConsoleTextAttribute(hConsole, 9);
+        std::cout << file + "/" << std::endl;
+    }
+    else if (endsWith(file, ".exe")) {
+        SetConsoleTextAttribute(hConsole, 12);
+        std::cout << file << std::endl;
+    }
+    else {
+        SetConsoleTextAttribute(hConsole, 15);
+        std::cout << file << std::endl;
+    }}
+
 int main(int argc, char* argv[]) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-    string path = fs::current_path().string();
+    std::string path = std::filesystem::current_path().string();
+
     if (argc == 1) {
-        for (const auto& entry : fs::directory_iterator(path)) {
-            file = entry.path().string();
-            file.erase(file.begin(), file.begin() + path.size() + 1);
-            if (fs::is_directory(entry)) {
-                SetConsoleTextAttribute(hConsole, 9);
-                cout << file + "/" << endl;
+        for (const auto& entry : std::filesystem::directory_iterator(path)) {
+            listFiles(entry, path, hConsole);
+        }
+        SetConsoleTextAttribute(hConsole, 15);
+    }
+    else {
+        bool rec = false;
+        bool help = false;
+        for(int i = 1; i <= argc; i++)
+        {
+            if ((strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) && !help)  {
+                help = true;
+                std::cout << "Usage: ls [FILE]" << std::endl;
+                std::cout << "List files in a directory" << std::endl;
+                std::cout << "Source code: https://github.com/NotKronos/winls" << std::endl;
             }
-            else if (endsWith(file, ".exe")) {
-                SetConsoleTextAttribute(hConsole, 12);
-                cout << file << endl;
-            }
-            else {
+            if ((strcmp(argv[i], "--recursive") == 0 || strcmp(argv[i], "-R") == 0) && !rec) {
+                rec = true;
+                for (const auto& entry : std::filesystem::recursive_directory_iterator(path)) {
+                        listFiles(entry, path, hConsole);
+                }
                 SetConsoleTextAttribute(hConsole, 15);
-                cout << file << endl;
             }
 
         }
     }
-    else if (strcmp(argv[1], "--help") == 0 || strcmp(argv[1], "-h") == 0) {
-        cout << "Usage: ls [FILE]" << endl;
-        cout << "List files in a directory" << endl;
-        cout << "Source code: https://github.com/NotKronos/winls" << endl;
-    }
-    else if (strcmp(argv[1], "--recursive") == 0 || strcmp(argv[1], "-R") == 0) {
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        string path = fs::current_path().string();
-        for (const auto& entry : fs::recursive_directory_iterator(path)) {
-            file = entry.path().string();
-            file.erase(file.begin(), file.begin() + path.size() + 1);
-            if (fs::is_directory(entry)) {
-                SetConsoleTextAttribute(hConsole, 9);
-                cout << file + "/" << endl;
-            }
-            else if (endsWith(file, ".exe")) {
-                SetConsoleTextAttribute(hConsole, 12);
-                cout << file << endl;
-            }
-            else {
-                SetConsoleTextAttribute(hConsole, 15);
-                cout << file << endl;
-            }
-
-        }
-    }
-    SetConsoleTextAttribute(hConsole, 15);
 }
 
